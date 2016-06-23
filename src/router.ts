54f178f3dfbd14a854c97a6547f707061fb6be9a
Takeo Asai <spitson+github@gmail.com>
2016-06-23 18:44:42 +0900

    Revert "Align automatically"
    This reverts commit ca01b230c083bd32cedebdf240104213957fb5b9.

--- src/router.ts
+++ src/router.ts
@@ -5,67 +5,67 @@ import AccountCtrl from "./account/account.controller";
 
 angular.module("router", ["ionic"])
 
-  .config(function ($stateProvider, $urlRouterProvider) {
+.config(function($stateProvider, $urlRouterProvider) {
 
-    // Ionic uses AngularUI Router which uses the concept of states
-    // Learn more here: https://github.com/angular-ui/ui-router
-    // Set up the various states which the app can be in.
-    // Each state's controller can be found in controllers.js
-    $stateProvider
+  // Ionic uses AngularUI Router which uses the concept of states
+  // Learn more here: https://github.com/angular-ui/ui-router
+  // Set up the various states which the app can be in.
+  // Each state's controller can be found in controllers.js
+  $stateProvider
 
-      // setup an abstract state for the tabs directive
-      .state("tab", {
-        url: "/tab",
-        abstract: true,
-        templateUrl: "templates/tabs.html"
-      })
+  // setup an abstract state for the tabs directive
+    .state("tab", {
+    url: "/tab",
+    abstract: true,
+    templateUrl: "templates/tabs.html"
+  })
 
-      // Each tab has its own nav history stack:
+  // Each tab has its own nav history stack:
 
-      .state("tab.dash", {
-        url: "/dash",
-        views: {
-          "tab-dash": {
-            templateUrl: "templates/tab-dash.html",
-            controller: DashCtrl,
-            controllerAs: "vm",
-          }
+  .state("tab.dash", {
+    url: "/dash",
+    views: {
+      "tab-dash": {
+        templateUrl: "templates/tab-dash.html",
+        controller: DashCtrl,
+        controllerAs: "vm",
+      }
+    }
+  })
+
+  .state("tab.chats", {
+      url: "/chats",
+      views: {
+        "tab-chats": {
+          templateUrl: "templates/tab-chats.html",
+          controller: ChatsCtrl,
+          controllerAs: "vm",
         }
-      })
-
-      .state("tab.chats", {
-        url: "/chats",
-        views: {
-          "tab-chats": {
-            templateUrl: "templates/tab-chats.html",
-            controller: ChatsCtrl,
-            controllerAs: "vm",
-          }
+      }
+    })
+    .state("tab.chat-detail", {
+      url: "/chats/:chatId",
+      views: {
+        "tab-chats": {
+          templateUrl: "templates/chat-detail.html",
+          controller: ChatDetailCtrl,
+          controllerAs: "vm",
         }
-      })
-      .state("tab.chat-detail", {
-        url: "/chats/:chatId",
-        views: {
-          "tab-chats": {
-            templateUrl: "templates/chat-detail.html",
-            controller: ChatDetailCtrl,
-            controllerAs: "vm",
-          }
-        }
-      })
+      }
+    })
 
-      .state("tab.account", {
-        url: "/account",
-        views: {
-          "tab-account": {
-            templateUrl: "templates/tab-account.html",
-            controller: AccountCtrl,
-            controllerAs: "vm",
-          }
-        }
-      });
-
-    // if none of the above states are matched, use this as the fallback
-    $urlRouterProvider.otherwise("/tab/dash");
-
+  .state("tab.account", {
+    url: "/account",
+    views: {
+      "tab-account": {
+        templateUrl: "templates/tab-account.html",
+        controller: AccountCtrl,
+        controllerAs: "vm",
+      }
+    }
   });
+
+  // if none of the above states are matched, use this as the fallback
+  $urlRouterProvider.otherwise("/tab/dash");
+
+});
