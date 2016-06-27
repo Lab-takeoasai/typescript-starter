3b5caacf511c0ff7e23e167727f2030f2daa3a62
Takeo Asai <spitson+github@gmail.com>
2016-06-27 18:46:55 +0900

    Implement E2E test

---
+++ src/chat/chats.controller.e2e.ts
@@ -0,0 +1,18 @@
+import { assert } from "chai";
+
+describe("Chat controller ", () => {
+
+  beforeEach(function() {
+    browser.get("/#/tab/chats");
+  });
+
+  it("(this is always true)", () => {
+    assert.isNull(null);
+  });
+
+  it("'s title is Chats", () => {
+    return browser.getTitle().then(title => {
+      assert.equal(title, "Chats");
+    });
+  });
+});
