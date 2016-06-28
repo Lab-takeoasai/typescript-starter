d5cc8ecd630237c7ac153ffd50ca3a45cb519a3e
Takeo Asai <spitson+github@gmail.com>
2016-06-28 18:49:58 +0900

    Add decorator sample

--- src/account/account.controller.ts
+++ src/account/account.controller.ts
@@ -1,3 +1,18 @@
+let debug = (target: any, name: string, descriptor: PropertyDescriptor) => {
+  let delegate = descriptor.value;
+  descriptor.value = function () {
+    let args: string[] = [];
+    for (let i = 0; i < arguments.length; i++) {
+      args.push(arguments[i]);
+    }
+    console.log(`${name} in: ${args.join()}`);
+    let result = delegate.apply(this, arguments);
+    console.log(`${name} out: ${result}`);
+    return result;
+  };
+  return descriptor;
+};
+
 export default class AccountController {
   public settings;
   constructor($scope) {
@@ -5,5 +20,10 @@ export default class AccountController {
     this.settings = {
       enableFriends: true
     };
+    this.add(1, 3);
+  }
+  @debug
+  public add(a, b) {
+    return a + b;
   }
 }
