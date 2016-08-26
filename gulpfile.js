c7f3f67cc0a34565a22f7136c6702473d1b0cea2
Takeo Asai <spitson+github@gmail.com>
2016-08-26 11:53:18 +0900

    Fix path

--- gulpfile.js
+++ gulpfile.js
@@ -119,7 +119,6 @@ gulp.task('git-check', function (done) {
       '\n  Download git here:', gutil.colors.cyan('http://git-scm.com/downloads') + '.',
       '\n  Once git is installed, run \'' + gutil.colors.cyan('gulp install') + '\' again.'
     );
-    process.exit(1);
   }
   done();
 });
