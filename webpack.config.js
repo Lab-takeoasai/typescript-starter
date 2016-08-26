1d5a89394c969d78cb6d7baed717a12beb8150fd
Takeo Asai <spitson+github@gmail.com>
2016-08-26 11:20:23 +0900

    Modify to pack ionic with webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,15 +4,20 @@ var replacements = require('./config/replacements.config');
 var WebpackNotifierPlugin = require('webpack-notifier');
 module.exports = {
   entry: {
-    app: ['./src/app.ts']
+    app: ['./src/app.ts'],
+    bundle: ['ionic/js/ionic.bundle.min.js', 'ngCordova/dist/ng-cordova.min.js'],
   },
   output: {
     filename: './www/js/[name].dist.js'
   },
   resolve: {
-    root: [path.join(__dirname, 'node_modules')],
+    root: [path.join(__dirname, 'node_modules'), path.join(__dirname, 'bower_components')],
     extensions: ['', '.ts', '.webpack.js', '.web.js', '.js']
   },
+  moduleDirectories: [
+    'bower_components',
+    'node_modules',
+  ],
   watch: true,
   devtool: (process.env.ENV === 'release') ? 'eval' : 'inline-source-map',
   module: {
